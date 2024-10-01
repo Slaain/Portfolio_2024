ecc7b53c25d6ae051e6f975336981c07f0d494fc
Ali <idris1390@gmail.com>
2024-10-01 23:42:21 +0200

    Updates

--- _app/immutable/chunks/paths.P5jK4oiJ.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1hm3os5)==null?void 0:s.base)??"/Portfolio_2024";var o;const a=((o=globalThis.__sveltekit_1hm3os5)==null?void 0:o.assets)??t;export{a,t as b};
