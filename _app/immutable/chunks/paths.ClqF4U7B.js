4e9d14961385c2ac2f3ff28c15e9b0bfe3714605
Slaain <92577893+Slaain@users.noreply.github.com>
2025-08-28 14:49:24 +0000

    Deploying to gh-pages from @ Slaain/Portfolio_2024@234d55537f20a029d88e8c323b566e149223203b 🚀

---
+++ _app/immutable/chunks/paths.ClqF4U7B.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_qfy54q)==null?void 0:s.base)??"/Portfolio_2024";var t;const e=((t=globalThis.__sveltekit_qfy54q)==null?void 0:t.assets)??a;export{e as a,a as b};
